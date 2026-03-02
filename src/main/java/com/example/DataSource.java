4fdc9368ef22f2e019a93a234c147912769c91a2
Gyenge Elemér <gyenge@valahol>
2026-03-02 18:31:09 +0100

    Product hozzáadva

--- src/main/java/com/example/DataSource.java
+++ src/main/java/com/example/DataSource.java
@@ -1,14 +1,29 @@
 package com.example;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DataSource {
     Database database;
     public DataSource(Database database) {
         this.database = database;
     }
-    public void createProduct() {
+    public void createProduct(Product product) {
         Connection conn = database.connectDb();
+        String sql = """
+        insert into products
+        (name, price)
+        values
+        (?, ?)
+        """;
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        
     }
     public void readProducts() {}
     public void updateProduct() {}
